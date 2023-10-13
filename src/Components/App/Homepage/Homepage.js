640b5b12e1f288494ded78a3c566783f465ffd9a
Renee Pinna <renee.pinna@gmail.com>
2023-10-12 21:25:13 -0600

    Add Homepage component

---
+++ src/Components/App/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+function Homepage() {
+  return (
+    <>
+      <header className="home-header">
+        <NavBar />
+        <img className="hero-img"/>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default Homepage;
