4e0ae0b8e915c51e87baac6f317dd3f2d3808a40
Parvin Sattorova <nanajon66@gmail.com>
2023-10-13 22:32:45 -0600

    fix: Add fix fo  warning about babel-preset-react-app

--- src/Components/ProvideAidPage/ProvideAidPage.js
+++ src/Components/ProvideAidPage/ProvideAidPage.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import NavBar from '../NavBar/NavBar';
 import { useQuery } from '@apollo/client';
 import { gql } from '@apollo/client';
+import '../ProvideAidPage/ProvideAidPage.scss';
+
 
 const GET_ORGANIZATIONS = gql`
     query {
@@ -58,50 +60,74 @@ const ProvideAidPage = () => {
 
         const foundRequest = aidRequests.find(req => req.description.toLowerCase().includes(value.toLowerCase()));
         setSelectedOrganization(foundRequest);
-        
+
     };
 
-
     return (
-        <div>
+        <div className="provide-aid-page">
             <NavBar />
-
-            <div>
-                <input
-                    type="text"
-                    value={searchTerm}
-                    onChange={handleSearchChange}
-                    placeholder="Search for organizations"
-                />
-                <ul>
-                    {example_aid_requests.map(item => (
-                        <li key={item.id}>
-                            <h3>{item.aidType}</h3>
-                            <p>{item.description}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-            <div>
-                {selectedOrganization && (
-                    <>
-                        <h2>{selectedOrganization.name}</h2>
-                        <p>Phone: {selectedOrganization.contactPhone}</p>
-                        <p>Email: {selectedOrganization.contactEmail}</p>
-                        <p>Address: {selectedOrganization.streetAddress}</p>
-                        <p>Website: {selectedOrganization.website}</p>
-                        <p>City: {selectedOrganization.city}</p>
-                        <p>State: {selectedOrganization.state}</p>
-                        <p>ZIP: {selectedOrganization.zip}</p>
-                        <div style={{ width: '300px', height: '300px', backgroundColor: 'gray' }}>
-                            Map Here (Integration with map service required)
-                        </div>
-                    </>
-                )}
+    
+            <div className="content-section">
+    
+                {/* Left Section */}
+                <div className="left-section">
+                    <div className="search-section">
+                        <input
+                            type="text"
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                            placeholder="Search for organizations"
+                        />
+                        <button onClick={handleSearchChange}>Find</button>
+                    </div>
+    
+                    <div className="filter-fields">
+                        <select>
+                            <option value="filter1">Filter 1</option>
+                            <option value="filter2">Filter 2</option>
+                            <option value="filter3">Filter 3</option>
+                        </select>
+                        <select>
+                            <option value="filterA">Filter A</option>
+                            <option value="filterB">Filter B</option>
+                            <option value="filterC">Filter C</option>
+                        </select>
+                    </div>
+    
+                    <ul>
+                        {example_aid_requests.map(item => (
+                            <li key={item.id}>
+                                <h3>{item.aidType}</h3>
+                                <p>{item.description}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+    
+                {/* Right Section */}
+                <div className="right-section">
+                    {selectedOrganization && (
+                        <>
+                            <h2>{selectedOrganization.name}</h2>
+                            <p>Phone: {selectedOrganization.contactPhone}</p>
+                            <p>Email: {selectedOrganization.contactEmail}</p>
+                            <p>Address: {selectedOrganization.streetAddress}</p>
+                            <p>Website: {selectedOrganization.website}</p>
+                            <p>City: {selectedOrganization.city}</p>
+                            <p>State: {selectedOrganization.state}</p>
+                            <p>ZIP: {selectedOrganization.zip}</p>
+                            <div style={{ width: '300px', height: '300px', backgroundColor: 'gray' }}>
+                                Map Here (Integration with map service required)
+                            </div>
+                        </>
+                    )}
+                </div>
+    
             </div>
         </div>
     );
+    
+
 };
 
 export default ProvideAidPage;
