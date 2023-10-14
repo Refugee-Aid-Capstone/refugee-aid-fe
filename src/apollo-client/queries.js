c7068959b22a28cc00cd60c7b0edd6b805e528a4
Renee Pinna <renee.pinna@gmail.com>
2023-10-14 14:18:10 -0600

    Add queries

--- src/apollo-client/queries.js
+++ src/apollo-client/queries.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
-export const GET_ORG_REQUESTS = gql`
-  query {
-    organization(id: 1) {
+export const GET_ONE_ORG = gql`
+  query getOneOrg($id: Integer!) {
+    organization(byId: $id) {
       id
       name
       contactPhone
@@ -25,6 +25,48 @@ export const GET_ORG_REQUESTS = gql`
         description
         status
       }
+    }
+  }
+`;
+
+const GET_ALL_ORGS_BY_AREA = gql`
+  query getAllOrgs($city: String!, $state: String!) {
+    organizations(city: $city, state: $state) {
+      id
+      name
+      contactPhone
+      contactEmail
+      streetAddress
+      website
+      city
+      state
+      zip
+      latitude
+      longitude
+      shareAddress
+      sharePhone
+      shareEmail
+      aidRequests {
+        id
+        organizationId
+        aidType
+        language
+        description
+        status
+      }
+    }
+  }
+`;
+
+const GET_ALL_REQUESTS_BY_AREA = gql`
+  query getAllRequestByArea($city: String!, $state: String!) {
+    requests(city: $city, state: $state) {
+      id
+      organizationId
+      aidType
+      language
+      description
+      status
     }
   }
 `;
