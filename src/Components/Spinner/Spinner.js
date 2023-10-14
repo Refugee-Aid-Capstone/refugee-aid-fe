8f122aeaea4ce7cd467f3cb9f18762d54017cd52
Parvin Sattorova <nanajon66@gmail.com>
2023-10-14 17:50:33 -0600

    feature:Add spinner functionality and style

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './Spinner.scss';
 
 export default function Spinner() {
   return (
-    <div>
-      
+    <div className="spinner-overlay">
+      <div className="spinner"></div>
     </div>
-  )
+  );
 }
+
+
+// import tree from "../../images/pine.png";
+// import "./LoadingComponent.css";
+
+// function LoadingComponent() {
+//   return (
+//     <div className="LoadingComponent">
+//       <header className="LoadingComponent-header">
+//         <img src={tree} className="LoadingComponent-logo" alt="tree" />
+//         <p>Loading...</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default LoadingComponent;
