17e121c8e196180cecf686587f2f6b54ffacda3a
Parvin Sattorova <nanajon66@gmail.com>
2023-10-15 12:12:11 -0600

    refactore: Add edit to nav bar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,16 @@
-import './NavBar.scss'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import './NavBar.scss'; 
+import { NavLink } from 'react-router-dom';
+import logoImage from '../../images/refuge.png';
 
 export default function NavBar() {
   return (
     <nav className='nav-bar'>
-      <h1>REFUGE</h1>
-      <NavLink className='nav-link' to='/provideAid'>Provide Aid</NavLink>
-      <NavLink className='nav-link' to='/ourMission'>Our Mission</NavLink>
+      <div className="navbar-logo">
+        <img src={logoImage} alt="Logo" />
+      </div>
+      <NavLink className='nav-link' to='/provideAid'>Aid</NavLink>
+      <NavLink className='nav-link' to='/ourMission'>Mission</NavLink>
     </nav>
-  )
+  );
 }
