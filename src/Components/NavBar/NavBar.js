dba608bfe27f36dc5a3564d1150ad1a613f2bbb2
Parvin Sattorova <nanajon66@gmail.com>
2023-10-16 14:04:33 -0600

    feature:Add video function and style fix in Nav Bar componnent

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -12,7 +12,7 @@ export default function NavBar() {
         </NavLink>
       </div>
       <NavLink className='nav-link' to='/provideAid'>Aid</NavLink>
-      <NavLink className='nav-link' to='/ourMission'>Mission</NavLink>
+      {/* <NavLink className='nav-link' to='/ourMission'>Mission</NavLink> */}
     </nav>
   );
 }
