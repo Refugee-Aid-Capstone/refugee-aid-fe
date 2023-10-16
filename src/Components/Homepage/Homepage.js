d1f7e40492cffc98ab4c212d3c441e7e933123e5
Parvin Sattorova <nanajon66@gmail.com>
2023-10-15 19:01:56 -0600

    feature: Add a link to logo so it is taking user back to the home page. add an img and add styles

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,12 +1,14 @@
+import refugeesImage from '../../images/refugees.png';
 import NavBar from "../NavBar/NavBar";
 
+
+
 function Homepage() {
-  
   return (
     <>
       <header className="home-header">
         <NavBar />
-        {/* <img className="hero-img"/> */}
+        <img src={refugeesImage} alt="Refugees" className="refugees-img" />
       </header>
       <main></main>
     </>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './NavBar.scss'; 
 import { NavLink } from 'react-router-dom';
-import logoImage from '../../images/refuge.png';
+import logoImage from '../../images/refuge.PNG';
 
 export default function NavBar() {
   return (
     <nav className='nav-bar'>
       <div className="navbar-logo">
-        <img src={logoImage} alt="Logo" />
+        <NavLink to='/'>
+          <img src={logoImage} alt="Logo" />
+        </NavLink>
       </div>
       <NavLink className='nav-link' to='/provideAid'>Aid</NavLink>
       <NavLink className='nav-link' to='/ourMission'>Mission</NavLink>
