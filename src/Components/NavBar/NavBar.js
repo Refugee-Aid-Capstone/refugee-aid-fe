2dc796af6704e6013ba716080e9c753956177d5b
Parvin Sattorova <nanajon66@gmail.com>
2023-10-17 18:09:32 -0600

    feature: Add login for organization dashboard

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import './NavBar.scss'; 
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import './NavBar.scss';
+import { NavLink, useNavigate } from 'react-router-dom';
 import logoImage from '../../images/refuge.png';
 
 export default function NavBar() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/organizationDashboard');
+  }
+
+
   return (
     <nav className='nav-bar'>
       <div className="navbar-logo">
-        <NavLink to='/'>
-          <img src={logoImage} alt="Logo" />
-        </NavLink>
-      </div>
-      <NavLink className='nav-link' to='/provideAid'>aid reguest</NavLink>
-      <NavLink className='nav-link' to='/'>donate</NavLink>
-      <NavLink className='nav-link' to='/OrganizationDashboard'>organization</NavLink> 
-      <NavLink className='nav-link' to='/'>mission</NavLink>
+  <NavLink to='/'>
+    <img src={logoImage} alt="Logo" />
+  </NavLink>
+</div>
+<div className="nav-items">
+  <NavLink className='nav-link' to='/provideAid'>aid reguest</NavLink>
+  <NavLink className='nav-link' to='/'>donate</NavLink>
+  <NavLink className='nav-link' to='/OrganizationDashboard'>organization</NavLink> 
+  <NavLink className='nav-link' to='/'>mission</NavLink>
+</div>
+<div className="login-container">  
+  {loggedIn ? (
+    <>
+      <NavLink className='nav-link' to='/organizationDashboard'>Dashboard</NavLink>
+      <button onClick={() => setLoggedIn(false)}>Logout</button>
+    </>
+  ) : (
+    <button className="nav-link login-button" onClick={handleLogin}>login</button>
+  )}
+</div>
     </nav>
   );
-}
+}  
