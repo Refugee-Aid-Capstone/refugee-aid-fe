b38ea9574485028ca8cda4468303e14a71985b98
Renee Pinna <renee.pinna@gmail.com>
2023-10-20 19:41:53 -0600

    Add map obscuration

--- src/Components/OrganizationDisplay/OrganizationDisplay.js
+++ src/Components/OrganizationDisplay/OrganizationDisplay.js
@@ -1,40 +1,58 @@
 import React from 'react';
-import welcomeImage from '../../images/welcome-usa-refugee.png'
+import welcomeImage from '../../images/welcome-usa-refugee.png';
 import Map from '../../Map/Map';
 
 export default function OrganizationDisplay({ selectedOrganization }) {
-  const {latitude, longitude} = selectedOrganization
+  const {
+    name,
+    contactPhone,
+    contactEmail,
+    streetAddress,
+    website,
+    city,
+    state,
+    zip,
+    latitude,
+    longitude,
+    shareAddress,
+    sharePhone,
+    shareEmail,
+  } = selectedOrganization;
 
   return (
     <div className='organization-display'>
       {selectedOrganization.name ? (
         <>
-          <h2>{selectedOrganization.name}</h2>
-          <p>Phone: {selectedOrganization.contactPhone}</p>
-          <p>Email: {selectedOrganization.contactEmail}</p>
-          <p>Address: {selectedOrganization.streetAddress}</p>
-          <p>Website: {selectedOrganization.website}</p>
-          <p>City: {selectedOrganization.city}</p>
-          <p>State: {selectedOrganization.state}</p>
-          <p>ZIP: {selectedOrganization.zip}</p>
+          <h2>{name}</h2>
+          {sharePhone && <p>Phone: {contactPhone}</p>}
+          {shareEmail && <p>Email: {contactEmail}</p>}
+          {shareAddress && <p>Address: {streetAddress}</p>}
+          <p>Website: {website}</p>
+          <p>City: {city}</p>
+          <p>State: {state}</p>
+          <p>ZIP: {zip}</p>
           <div
             style={{
               height: '300px',
             }}
           >
-            <Map latitude={latitude} longitude={longitude} selectedOrganization={selectedOrganization}/>
-            {' '}
+            <Map
+              latitude={latitude}
+              longitude={longitude}
+              selectedOrganization={selectedOrganization}
+              shareAddress={shareAddress}
+            />{' '}
           </div>
         </>
       ) : (
-                    <div className='welcome-image-container'>
-                      <img
-                        src={welcomeImage}
-                        alt='welcome-usa-refugee'
-                        className='welcome-image'
-                      />
-                    </div>
-                  )}
+        <div className='welcome-image-container'>
+          <img
+            src={welcomeImage}
+            alt='welcome-usa-refugee'
+            className='welcome-image'
+          />
+        </div>
+      )}
     </div>
   );
 }
