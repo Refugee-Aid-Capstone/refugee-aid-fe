df786234eebe61316d1645b5ae593e1850341db6
Parvin Sattorova <nanajon66@gmail.com>
2023-11-16 08:33:13 -0700

    Add org create request button

--- src/Components/CareRequestForm/CareRequestForm.js
+++ src/Components/CareRequestForm/CareRequestForm.js
@@ -7,7 +7,7 @@ function CareRequestForm() {
     const [formData, setFormData] = useState({
         aidType: '',
         description: '',
-        // You can add more fields as needed
+        // more fields ?
     });
 
     const handleChange = (e) => {
@@ -20,11 +20,11 @@ function CareRequestForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Here you should add the logic to send data to your server
+        // Here we should add the logic to send data to the server
         // For now, we'll just log the formData and navigate to a different page
         console.log(formData);
 
-        // After submitting, navigate to a different page, e.g., the dashboard or a success page
+        // After submitting, navigate to a different page, mayby the dashboard or a success page?
         navigate('/success'); 
     };
 
@@ -37,14 +37,12 @@ function CareRequestForm() {
                         <option value="">Select Aid Type</option>
                         <option value="food">Food</option>
                         <option value="medical">Medical</option>
-                        {/* Add other options as necessary */}
                     </select>
                 </div>
                 <div className="form-group">
                     <label htmlFor="description">Description:</label>
                     <textarea name="description" value={formData.description} onChange={handleChange} className="form-control" />
                 </div>
-                {/* Add other input fields similarly */}
                 <button type="submit" className="submit-button">Submit Request</button>
             </form>
         </div>
